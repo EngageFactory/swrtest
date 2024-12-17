2c1105ffe1d897b7bfb7a53bf1811273eee5ec6e
Marko Bratkovič <marko.bratkovic@kreatorij.si>
2024-12-17 16:44:55 +0100

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,9 @@
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
     devtools: { enabled: true },
-    nitro: {
-        preset: 'netlify-builder',
-    },
+    // nitro: {
+    //     preset: 'netlify-builder',
+    // },
     routeRules: {
         '/api/**': { cache: false, swr: false, cors: true, prerender: false },
     },
